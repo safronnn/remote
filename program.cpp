0185af19a7991c980551dbbf77fc4fd7fcee7a9f
Fedor <f.safronov2013@yandex.ru>
2022-12-14 01:14:23 +0300

    add function MyPow() v2

--- program.cpp
+++ program.cpp
@@ -1,13 +1,16 @@
 //פאיכ program.cpp
 #include <iostream>
 #include "funcs.h"
+
 using namespace std;
+
 int main()
 {
-cout << MySum(10, 3) << endl;
-cout << MySub(10, 3) << endl;
-cout << MyMul(10, 3) << endl;
-cout << MyDiv(10, 3) << endl;
-system('pause');
-return 0;
+    cout << MySum(10, 3) << endl;
+    cout << MySub(10, 3) << endl;
+    cout << MyMul(10, 3) << endl;
+    cout << MyDiv(10, 3) << endl;
+    cout << MyPow(10.5, 3.7) << endl;
+    system('pause');
+    return 0;
 }
